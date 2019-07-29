a064b909d387921c0fb94e6c495c9d1d8545c365
kpassaglia <kpassaglia@gmail.com>
2019-07-28 22:02:32 -0500

    Updates

---
+++ precache-manifest.34e45b29c27eec34db5087bddcdcea32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3b8da694cedb7bd840c64fe2e37e75",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "88c59944a9c08ad07fe5",
+    "url": "/ClickyGame/static/css/main.3d5166fd.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/ClickyGame/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "88c59944a9c08ad07fe5",
+    "url": "/ClickyGame/static/js/main.e06ea317.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
